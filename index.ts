a5ba201267e756080c8d77d3d787636d775c739c
KienHa05 <kienhgph50150@gmail.com>
2025-02-27 15:43:24 +0700

    2.2 Tạo các routes cơ bản
    BUỔI 53

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("OK");
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+const port: number = 3000;
+
+app.get("/topics", (req: Request, res: Response) => {
+    res.send("Chủ Đề Bài Hát");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
