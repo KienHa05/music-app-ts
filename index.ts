5174b10140d5f8bf8cbcf36f5e286acdb2a4bc52
KienHa05 <kienhgph50150@gmail.com>
2025-02-28 21:58:59 +0700

    2. Layout chung bên Admin
    BUỔI 56

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
+import path from "path";
 import * as database from "./config/database";
 
+import adminRoutes from "./routes/admin/index.route";
 import clientRoutes from "./routes/client/index.route";
+import { systemConfig } from "./config/config";
 
 dotenv.config();
 
@@ -15,6 +18,19 @@ app.use(express.static("public"));
 
 app.set("views", "./views");
 app.set("view engine", "pug");
+
+// TinyMCE
+app.use(
+    '/tinymce',
+    express.static(path.join(__dirname, 'node_modules', 'tinymce'))
+);
+// End TinyMCE
+
+// App Local Variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
+
+// Admin Routes
+adminRoutes(app);
 
 // Client Routes
 clientRoutes(app);
