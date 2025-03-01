9645ecb0b71c7d82df432fa360c18b5b27f49de4
KienHa05 <kienhgph50150@gmail.com>
2025-03-01 19:56:50 +0700

    Create folder dist

---
+++ dist/models/topic.model.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose = require("mongoose");
+const topicSchema = new mongoose.Schema({
+    title: String,
+    avatar: String,
+    description: String,
+    status: String,
+    slug: String,
+    deleted: {
+        type: Boolean,
+        default: false,
+    },
+    deletedAt: Date,
+}, {
+    timestamps: true
+});
+const Topic = mongoose.model('Topic', topicSchema, "topics");
+exports.default = Topic;
