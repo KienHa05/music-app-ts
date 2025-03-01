5d379336912f650a4c4f8ad325423f1e4f9d14ed
KienHa05 <kienhgph50150@gmail.com>
2025-03-01 15:34:48 +0700

    3. Chỉnh Sửa Bài Hát
    BUỔI 57
    riêng phần edit lyrics thì có cả cộng dồng người dev rồi nên ta tự tìm rồi copy vào hoặc tự điền.

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
 import path from "path";
+import methodOverride from "method-override";
 import * as database from "./config/database";
 
 import adminRoutes from "./routes/admin/index.route";
@@ -15,7 +16,9 @@ const app: Express = express();
 const port: (number | string) = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(methodOverride("_method"));
 
 app.use(express.static("public"));
 
